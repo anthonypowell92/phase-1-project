e66a74f22e674074aada7c0126ede92c3d950f30
AnthonyPowell <anthonydewpow@gmail.com>
2023-02-04 20:26:00 -0600

    moving images into their own folder, re-coding the buttons and their id's, re-coding the event listeners to a different type of event listener

--- Project code/index.js
+++ Project code/index.js
@@ -1,11 +1,15 @@
-function changeImage(fileName) {
-  let img = document.querySelector("#tony-pic5");
-  img.setAttribute("src", fileName);
-}
+let img = document.querySelector('img')
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
 
+btn1.addEventListener('click', () => {
+    img.src = 'images/1.jpeg';
+})
+btn2.addEventListener('click', () => {
+  img.src = 'images/0.jpeg';
+})
 
-
-  document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     let form = document.querySelector("#create-task-form")
     form.addEventListener("submit", newTask)
   });
