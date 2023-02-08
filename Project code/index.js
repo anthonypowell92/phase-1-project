42934605fb01054cf0d70c77d51ac4f7d9f7f39e
AnthonyPowell <anthonydewpow@gmail.com>
2023-02-07 22:40:53 -0600

    trying onload event listener in html for script and trying return on functions in js to see if they respond

--- Project code/index.js
+++ Project code/index.js
@@ -2,18 +2,24 @@ let img = document.querySelector('#img')
 let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2');
 
+function ChangePic(){
 btn1.addEventListener('click', () => {
     img.src = 'images/1.jpeg';
 })
 btn2.addEventListener('click', () => {
   img.src = 'images/0.jpeg';
 })
+return ChangePic;
+}
 
 let form = document.querySelector("#create-task-form")
 
+function VigSubmit() {
 document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", newTask);
   });
+  return VigSubmit;
+}
 
   const EMPTY_HEART = '♡'
   const FULL_HEART = '♥'
@@ -21,12 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("DOMContentLoaded", () => {
 
       const hearts = document.querySelectorAll("span.like-glyph")
-      console.log(hearts)
 
       hearts.forEach(hearts => hearts.addEventListener("click", callB))
 
       function callB(hearts) {
-        console.log(hearts.target)
         mimicServerCall()
         .then( () => {
           if (hearts.target.innerText === EMPTY_HEART) {
@@ -37,6 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
             hearts.target.classList.remove('activated-heart')
               hearts.target.innerText = EMPTY_HEART
           }
+          return hearts.target;
         })
       }}
     )
