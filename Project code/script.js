57105b70aeb8abd59476e2df5550f7571f2919cf
AnthonyPowell <anthonydewpow@gmail.com>
2023-02-12 19:01:39 -0600

    add event listener to search input, add element to hide other names when search does not match

--- Project code/script.js
+++ Project code/script.js
@@ -1,18 +1,29 @@
-function allOfTheCocktails(){
-  fetch('http://localhost:3000/drinks')
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible =
+     user.strDrink.toLowerCase().includes(value) ||
+     user.strCategory.toLowerCase().includes(value)
+  user.element.classList.toggle("hide", !isVisible)
+  })
+})
+
+fetch('http://localhost:3000/drinks')
   .then(res => res.json())
-  .then(drinks => drinks.forEach(cocktail => renderOneCocktail(cocktail)))
-}
-allOfTheCocktails()
-
-function searchBar(){
-  allOfTheCocktails()
-  document.querySelector("searchBar").addEventListener('search', (event) => {});
-  onsearch = (event) => {};
-}
-searchBar()
-
-function searchButton(){
-  document.querySelector("searchButton").addEventListener('click', (event) => {});
-}
-searchButton()
+  .then(data => {
+   users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-header]")
+      header.textContent = user.strDrink
+      body.textContent = user.strCategory
+      userCardContainer.append(card)
+      return {name: user.strDrink, category: user.strCategory, element: card}
+  })
+})
